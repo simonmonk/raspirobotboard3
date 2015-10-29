3ae8447d05ee4914d35560bfb0f4636b21e839f8
Simon Monk <Si@Simons-Mac-Pro.local>
2015-10-29 13:56:57 +0000

    added support for stepper motor

--- python/rrb3.py
+++ python/rrb3.py
@@ -114,29 +114,29 @@ class RRB3:
             time.sleep(seconds)
             self.stop()
             
-    def step_forward(delay, num_steps):
+    def step_forward(self, delay, num_steps):
         for i in range(0, num_steps):
-            this.set_driver_pins(1, 1, 1, 0)
+            self.set_driver_pins(1, 1, 1, 0)
             time.sleep(delay)
-            this.set_driver_pins(1, 1, 1, 1)
+            self.set_driver_pins(1, 1, 1, 1)
             time.sleep(delay)
-            this.set_driver_pins(1, 0, 1, 1)
+            self.set_driver_pins(1, 0, 1, 1)
             time.sleep(delay)
-            this.set_driver_pins(1, 0, 1, 0)
+            self.set_driver_pins(1, 0, 1, 0)
             time.sleep(delay)
-        this.set_driver_pins(0, 0, 0, 0)
+        self.set_driver_pins(0, 0, 0, 0)
     
-    def step_reverse(delay, num_steps):
+    def step_reverse(self, delay, num_steps):
         for i in range(0, num_steps):
-            this.set_driver_pins(1, 0, 1, 0)
+            self.set_driver_pins(1, 0, 1, 0)
             time.sleep(delay)
-            this.set_driver_pins(1, 0, 1, 1)
+            self.set_driver_pins(1, 0, 1, 1)
             time.sleep(delay)
-            this.set_driver_pins(1, 1, 1, 1)
+            self.set_driver_pins(1, 1, 1, 1)
             time.sleep(delay)
-            this.set_driver_pins(1, 1, 1, 0)
+            self.set_driver_pins(1, 1, 1, 0)
             time.sleep(delay)
-        this.set_driver_pins(0, 0, 0, 0)
+        self.set_driver_pins(0, 0, 0, 0)
 
     def sw1_closed(self):
         return not GPIO.input(self.SW1_PIN)
